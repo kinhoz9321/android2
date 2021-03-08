91e0724796c0c24837dd458d18428fece19d5f2b
kimhongju <kinhoz9321@gmail.com>
2021-03-08 11:21:17 +0900

    step02activity 추가

---
+++ step02activity/src/main/java/com/example/step02activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.step02activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //add 실험 중
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
